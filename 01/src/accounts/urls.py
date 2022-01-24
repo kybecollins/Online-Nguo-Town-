0d33e8f4c7419426c4e4111770d54a19436c72b5
kybecollins <colkibe@gmail.com>
2022-01-24 16:37:26 +0300

    small authentication update
    worked on the authentication of users

--- 01/src/accounts/urls.py
+++ 01/src/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from accounts.views import logout_default_view,login_default_view
+from accounts.views import logout_default_view,login_default_view,registration_default_view
 
 
 app = "accounts"
@@ -9,4 +9,5 @@ urlpatterns = [
     # path('login/' ),
     path('logout/',logout_default_view,name ='logout_view'),
     path('login/',login_default_view,name ='login_view'),
+    path('register/',registration_default_view,name ='register_view'),
 ]
--- 01/src/accounts/views.py
+++ 01/src/accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,HttpResponseRedirect
 from django.contrib.auth import logout,login,authenticate
 
-from accounts.forms import LoginForm
+from accounts.forms import LoginForm,RegistrationForm
 
 # Create your views here.
 def logout_default_view(request):
@@ -19,5 +19,23 @@ def login_default_view(request):
         login(request,user)
         
     context ={'form':form}
+
+    return render(request,"accounts/login.html",context)
+
+def registration_default_view(request):
+    form = RegistrationForm(request.POST or None)
+
+    if form.is_valid():
+        print("Is Valid")
+        new_user = form.save(commit=False)
+        # username =form.cleaned_data['username']
+        # password = form.cleaned_data['password']
+        # user = authenticate (username =username, password=password)
+        # login(request,user)
+        
+        
+    context ={
+        'form':form
+        }
 
     return render(request,"accounts/login.html",context)
